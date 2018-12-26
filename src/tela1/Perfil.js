3861414158d66c87634b9a1bdd5e9babe5fe831b
Thalynny Rabelo <Thalynny.Rabelo@keyrus.com.br>
2018-12-26 17:24:47 -0200

    Construcao do Metas

--- src/tela1/Perfil.js
+++ src/tela1/Perfil.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Metas from './Metas'
 
 
 const styles = theme => ({
@@ -18,22 +20,74 @@ const styles = theme => ({
 });
 
 function FullWidthGrid(props) {
-    const { classes } = props;
+    // const { classes } = props;
 
     return (
         <div className="card" container>
             <Grid container spacing={20}>
-                <div item xs={4} className="avatar">
-                    <img alt="Remy Sharp" className="avatarInt" src="https://i.pinimg.com/564x/72/c3/56/72c35655c647e3b792aeb3dfa09a5caa.jpg"/>
-                   
+                <div item xs={12} sm={3} className="avatar">
+                    <img alt="Remy Sharp" className="avatarInt" src="https://i.pinimg.com/564x/72/c3/56/72c35655c647e3b792aeb3dfa09a5caa.jpg" />
                 </div>
-                <Grid item xs={4} >
-                    Teste2
-          {/* <Paper className={classes.paper}>xs=12 sm=6</Paper> */}
+                <div className="imagem">
+                    <div class="imagem-mascara"></div>
+                </div>
+
+                <Grid item xs={12} sm={4} >
+                    <div className="descricao">
+                        <Typography item variant="h4" gutterBottom>
+                            João Paulo Critis
+                        </Typography>
+                        <Typography variant="body2" gutterBottom>
+                            Head of Design,
+                            30 Anos.
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Tempo de KPMG : <strong>5 anos</strong>
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Tempo no cargo: <strong>2 anos</strong>
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Formação acadêmica:
+                          <ul class="circle">
+                                <li><strong>Universidade Federal do ABC</strong></li>
+                                Mestrado em Desing | (2017 - presente)
+                            <li><strong>Universidade Federal Rural da Amazônia</strong></li>
+                                Graduação em Sistemas de Informação | (2014 - 2016)
+                            <li><strong>Universidade Federal do Pará</strong></li>
+                                Pós Graduação em Direção de Arte (2015 - 2016)
+                            <li><strong>Universidade de São Paulo</strong></li>
+                                Técnico em Comunicação social | (1998 - 2002)
+                         </ul>
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            TOEIC: <strong> 720 </strong> (2018) | <strong> 700 </strong> (2017) | <strong> 610 </strong> (2016)
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Habilidade: <strong> OAB </strong> (Aprovado) | <strong> CRC </strong> (Válido)
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Mobility Realizado: <strong> ONova York 2015, Berlim 2017. </strong> (Aprovado) | <strong> CRC </strong> (Válido)
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Interesse em Mobility: <strong> Internacional / Nacional </strong>
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Aconselhados: <strong> 0 </strong>
+                        </Typography>
+                    </div>
+                    {/* <Paper className={classes.paper}>xs=12 sm=6</Paper> */}
                 </Grid>
-                <Grid item xs={4}>
-                    Teste3
-          {/* <Paper className={classes.paper}>xs=12 sm=6</Paper> */}
+                <Grid item xs={12} sm={5}>
+                   
+                        <div className="descricao">
+                        <Typography variant="h5" gutterBottom>
+                            <strong>Metas</strong>
+                            <Metas />
+                            </Typography>
+                        </div>
+                   
+                    {/* <Paper className={classes.paper}>xs=12 sm=6</Paper> */}
                 </Grid>
             </Grid>
         </div>
